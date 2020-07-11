3e25219f085dae83406f9fb3a149717219d46db0
Ray <balafranzraymond@gmail.com>
2020-07-12 01:19:30 +0800

    Added form row col import using BS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { Button, Form, Row, Col } from 'react-bootstrap';
+// import { Table } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="">
       <header className="">
-        <div className="topnav">
-          <a class="title">Task Scheduler App</a>
+        <div className="topnav my-3">
+          <a className="title">BMI and Energy Calculator</a>
         </div>
       </header>
-      <table className="">
-        <thead>
-            <tr>
-              <td>Number</td>
-              <td>Task</td>
-            </tr>
-        </thead>
-        <tbody>
+      <div className="mx-1 col-sm-6 col-md-6 col-lg-8">
+        <Form>
+          <Form.Group as={Row} controlId="formHorizontalEmail">
+            <Form.Label column sm={2}>
+              Height
+            </Form.Label>
+            <Col sm={4}>
+              <Form.Control type="email" placeholder="Height" />
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row} controlId="formHorizontalEmail">
+            <Form.Label column sm={2}>
+              Weight
+            </Form.Label>
+            <Col sm={4}>
+              <Form.Control type="email" placeholder="Weight" />
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row} controlId="formHorizontalEmail">
+            <Form.Label column sm={2}>
+              Age
+            </Form.Label>
+            <Col sm={4}>
+              <Form.Control type="email" placeholder="Age" />
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row} controlId="formHorizontalEmail">
+            <Form.Label column sm={2}>
+              Gender
+            </Form.Label>
+            <Col sm={4}>
+              <Form.Control type="email" placeholder="Gender" />
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row} controlId="formHorizontalEmail">
+            <Form.Label column sm={2}>
+              Race
+            </Form.Label>
+            <Col sm={4}>
+              <Form.Control type="email" placeholder="Race" />
+            </Col>
+          </Form.Group>
 
-        </tbody>
-      </table>
+          <Button variant="primary" type="submit">
+            Calculate BMI
+          </Button>
+          <br></br>
+          <Button className="my-2" variant="primary" type="submit">
+            Calculate Energy
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 }
